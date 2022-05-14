4ba9d6eb0d74b7ab191c33065f709c392c9a2933
theParas <parassth63@gmail.com>
2022-05-14 22:39:37 +0545

    setup premake to work for skull and sandbox

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "Skull"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Skull"
+	location "Skull"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" ..outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" ..outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.19041.0"
+
+		defines
+		{
+			"SK_PLATFORM_WINDOWS",
+			"SK_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath}  ../bin/"  ..outputdir.. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "SK_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SK_RELEASE"
+		optimize "On"
+		
+	filter "configurations:Dist"
+		defines "SK_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" ..outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" ..outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Skull/vendor/spdlog/include",
+		"Skull/src"  
+	}
+
+	links
+	{
+		"Skull"
+	}
+	
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.19041.0"
+
+		defines
+		{
+			"SK_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "SK_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SK_RELEASE"
+		optimize  "On"
+		
+	filter "configurations:Dist"
+		defines "SK_DIST"
+		optimize "On"
