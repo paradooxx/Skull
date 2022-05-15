a6b4cddefe1eec10ed0e44afe0515e484d0725c0
theParas <parassth63@gmail.com>
2022-05-15 20:59:32 +0545

    updated Event.h

--- Skull/src/Skull/Events/Event.h
+++ Skull/src/Skull/Events/Event.h
@@ -30,7 +30,8 @@ namespace Skull
 								virtual EventType GetEventType() const override { return GetStaticType(); }\
 								virtual const char* GetName() const override { return #type;}
 
-#define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override GetCategoryFlags() { return category;}
+#define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
+
 	class SKULL_API Event
 	{
 		friend class EventDispatcher;
@@ -39,5 +40,38 @@ namespace Skull
 		virtual const char* GetName() const = 0;
 		virtual int GetCategoryName() const = 0;
 		virtual std::string ToString() const { return GetName(); }
+
+		inline bool IsInCategory(EventCategory category)
+		{
+			return GetCategoryFlags() & category;
+		}
+	protected:
+		bool m_Handle = false;		//to check if event is handled
 	};
+
+	class EventDispatcher
+	{
+		template<typename T>
+		using EventFn = std::function<bool(T&)>;
+	public:
+		EventDispatcher(Event& event) : m_Event(event) {}
+
+		template<typename T>
+		bool Dispatch(EventFn<T> func)
+		{
+			if (m_Event.GetEventType() == T::GetStaticType())
+			{
+				m_Event.m_Handle = func(*(t*)&m_Event);
+				return true;
+			}
+			return false;
+		}
+	private:
+		Event& m_Event;
+	};
+
+	inline std::ostream& operator<<(std::ostream& os, const Event& e)
+	{
+		return os << e.ToString();
+	}
 }
